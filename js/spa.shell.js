8341b6452b077cb218f2ff85bb025bea7e610937
lichenbuliren <617106268@qq.com>
2016-03-02 22:42:50 +0800

    update

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -2,40 +2,31 @@
  *  spa.shell.js
  *  Shell module for SPA
  */
-spa.shell = (function(){
+spa.shell = (function() {
     //----------- 模块变量 -----------
     // 静态配置值
     var configMap = {
-        main_html : String()
-             + '    <div class="spa-shell-head">'
-             + '        <div class="spa-shell-head-logo"></div>'
-             + '        <div class="spa-shell-head-acct"></div>'
-             + '        <div class="spa-shell-head-search"></div>'
-             + '    </div>'
-             + '    <div class="spa-shell-main">'
-             + '        <div class="spa-shell-main-nav"></div>'
-             + '        <div class="spa-shell-main-content"></div>'
-             + '    </div>'
-             + '        <div class="spa-shell-foot"></div>'
-             + '        <div class="spa-shell-chat"></div>'
-             + '        <div class="spa-shell-modal"></div>',
+        main_html: String() + '    <div class="spa-shell-head">' + '        <div class="spa-shell-head-logo"></div>' + '        <div class="spa-shell-head-acct"></div>' + '        <div class="spa-shell-head-search"></div>' + '    </div>' + '    <div class="spa-shell-main">' + '        <div class="spa-shell-main-nav"></div>' + '        <div class="spa-shell-main-content"></div>' + '    </div>' + '        <div class="spa-shell-foot"></div>' + '        <div class="spa-shell-chat"></div>' + '        <div class="spa-shell-modal"></div>',
         // 聊天栏模块配置信息
-        chat_extend_time: 1000,
+        chat_extend_time: 250,
         chat_retract_time: 300,
         chat_extend_height: 450,
-        chat_retract_height: 15
+        chat_retract_height: 15,
+        chat_extend_title: 'Click to retract',
+        chat_retract_title: 'Click to extend'
     };
 
     // 动态信息
     var stateMap = {
-        $container: null
+        $container: null,
+        is_chat_retract: true
     };
 
     // jquery 对象缓存
     var jqueryMap = {};
 
     // 模块变量，之后赋值
-    var setJqueryMap, toggleChat, initModule;
+    var setJqueryMap, toggleChat, onClickChat, initModule;
     //----------- 模块变量 -----------
 
 
@@ -44,25 +35,27 @@ spa.shell = (function(){
     //----------- Utility Methods -----------
 
     //-----------Begin DOM Methods -----------
-    setJqueryMap = function(){
+    setJqueryMap = function() {
         var $container = stateMap.$container;
         jqueryMap.$container = $container;
         jqueryMap.$chat = $container.find('.spa-shell-chat');
     };
 
-    toggleChat = function(do_extend,callback){
+    toggleChat = function(do_extend, callback) {
         var chatHeight = jqueryMap.$chat.height(),
             is_open = chatHeight == configMap.chat_extend_height,
             is_closed = chatHeight == configMap.chat_retract_height,
             is_sliding = !is_open && !is_closed;
 
-        if(is_sliding) return false;
+        if (is_sliding) return false;
 
         // 展开聊天窗口
         if (do_extend) {
             jqueryMap.$chat.animate({
                 height: configMap.chat_extend_height,
             }, configMap.chat_extend_time, function() {
+                jqueryMap.$chat.attr('title', configMap.chat_extend_title);
+                stateMap.is_chat_retract = false;
                 callback && callback();
             });
 
@@ -71,8 +64,10 @@ spa.shell = (function(){
 
         // 合上聊天窗口
         jqueryMap.$chat.animate({
-                height: configMap.chat_retract_height
-        }, configMap.chat_retract_time,function(){
+            height: configMap.chat_retract_height
+        }, configMap.chat_retract_time, function() {
+            jqueryMap.$chat.attr('title', configMap.chat_retract_title);
+            stateMap.is_chat_retract = true;
             callback && callback();
         });
 
@@ -83,25 +78,26 @@ spa.shell = (function(){
 
     //----------- Begin Event Handlers -----------
     // 为jquery事件处理函数保留区块
+    onClickChat = function() {
+        toggleChat(stateMap.is_chat_retract);
+        return false;
+    };
     //----------- End Event Handlers   -----------
 
     //----------- Begin Public Methods   -----------
     // 公开方法放在这里
-    initModule = function($container){
+    initModule = function($container) {
         stateMap.$container = $container;
         $container.html(configMap.main_html);
         setJqueryMap();
 
-        // test
-        setTimeout(function(){
-            toggleChat(true);
-        },3000);
-
-        setTimeout(function(){
-            toggleChat(false);
-        },5000);
+        // 初始化绑定事件
+        stateMap.is_chat_retract = true;
+        jqueryMap.$chat.attr('title', configMap.chat_retract_title).click(onClickChat);
     };
 
-    return { initModule: initModule};
+    return {
+        initModule: initModule
+    };
     //----------- End Public Methods     -----------
 })();
